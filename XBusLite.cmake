78ee1ed997cdcc0594df7af66d73f6498dc9b18e
JosephShen <joseph.smeng@gmail.com>
2017-03-20 20:09:05 +0800

    update XBusLite.cmake

--- XBusLite.cmake
+++ XBusLite.cmake
@@ -25,8 +25,10 @@ function(xbus_lite_add_client client_name)
     target_sources(${client_name}_client PRIVATE ${xbus_lite_dir}/src/XBus.cxx)
     target_sources(${client_name}_client PRIVATE ${xbus_lite_dir}/src/XBus.hxx)
 
-    target_include_directories(${client_name}_client PRIVATE ${xbus_lite_dir}/src)
-    target_compile_definitions(${client_name}_client PRIVATE XBUS_SOURCE_FOR_CLIENT_HOST)
+    target_include_directories(
+        ${client_name}_client PRIVATE ${xbus_lite_dir}/src)
+    target_compile_definitions(
+            ${client_name}_client PRIVATE XBUS_SOURCE_FOR_CLIENT_HOST)
 
     # TODO: add target check here??
     target_sources(${client_name} PRIVATE ${xbus_lite_dir}/src/XBus.cxx)
@@ -38,10 +40,26 @@ endfunction()
 
 
 # export function for public use
-function(xbus_lite_set_client_executable_dir client_name client_host_dir)
-    set_target_properties(${client_name}_client PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${client_host_dir})
-endfunction()
+function(xbus_lite_set_client client_name ARG_TYPE)
+    if(${ARG_TYPE} STREQUAL "HOST_DIR")
+        set_target_properties(${client_name}_client PROPERTIES
+                                    RUNTIME_OUTPUT_DIRECTORY ${ARGN})
+    elseif(${ARG_TYPE} STREQUAL "CLIENT_SRC_DIR")
+        get_target_property(CLIENT_HOST_DIR ${client_name}_client
+                                                RUNTIME_OUTPUT_DIRECTORY)
 
-function(xbus_lite_set_client_executable_name client_name client_host_name)
-    set_target_properties(${client_name}_client PROPERTIES RUNTIME_OUTPUT_NAME ${client_host_name})
+        file(TO_NATIVE_PATH ${CMAKE_CURRENT_BINARY_DIR}/${CLIENT_HOST_DIR} CLIENT_HOST_DIR)
+        file(TO_NATIVE_PATH ${ARGN} CLIENT_SRC_DIR)
+
+        if(WIN32)
+            execute_process(COMMAND mkdir ${CLIENT_HOST_DIR})
+            execute_process(COMMAND xcopy ${CLIENT_SRC_DIR} ${CLIENT_HOST_DIR} /E /Y)
+        else()
+
+        endif()
+
+    elseif(${ARG_TYPE} STREQUAL "EXECUABLE_NAME")
+        set_target_properties(${client_name}_client PROPERTIES
+                                            RUNTIME_OUTPUT_NAME ${ARGN})
+    endif()
 endfunction()
